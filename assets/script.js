b1e4aa285945ca1de70f8596b54f32d0b170546a
Anna <am-rabota@mail.ru>
2021-08-24 21:15:38 -0700

    timer added

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var timerEl = document.getElementById('countdown');
+var startBtn = document.getElementById('start');
+
+// Timer that counts down from 60
+function countdown() {
+  var timeLeft = 60;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function() {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = "Time: " + timeLeft;
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      displayMessage();
+    }
+  }, 1000);
+}
+
+startBtn.onclick = countdown;
